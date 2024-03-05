4f0960a075a6b238ec3c242b9dcd8b83fa0bf00d
seoyunZZang <p815000@naver.com>
2024-03-05 21:14:46 +0900

    2024-03-05

--- security-back/src/main/java/com/example/securityback/config/SecurityConfig.java
+++ security-back/src/main/java/com/example/securityback/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.securityback.config;
 
 
+import com.example.securityback.jwt.JWTFilter;
 import com.example.securityback.jwt.JWTUtil;
 import com.example.securityback.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.tomcat.Jar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity // Spring Security를 사용하는 웹 보안을 활성화
@@ -47,6 +53,28 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        // CORS 설정 시큐리티쪽 설정
+        http
+                .cors((cors) -> cors
+                        .configurationSource(new CorsConfigurationSource() {
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                                CorsConfiguration configuration = new CorsConfiguration();
+
+                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000")); // 3000 번대 포트를 허용한다는 것
+                                configuration.setAllowedMethods(Collections.singletonList("*")); // 모든 메소드 허용
+                                configuration.setAllowCredentials(true);
+                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                                configuration.setMaxAge(3600L);
+
+                                configuration.setExposedHeaders(Collections.singletonList("Authorization")); // JWT 를 Authorization 를 넣어서 반환할 것이기 때문에 이것도 허용한다는 걸 추가
+
+
+                                return configuration;
+                            }
+                        }));
+
         // csrf disable
         http
                 .csrf((auth) -> auth.disable()); // CSRF(Cross-Site Request Forgery) 보호 기능을 비활성화, CSRF 공격으로부터 보호받기 위함
@@ -61,6 +89,10 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/", "/join").permitAll() // 로그인, 루트 경로, 회원가입 경로는 모든 사용자에게 허용
                         .requestMatchers("/admin").hasRole("ADMIN") // "/admin" 경로는 ADMIN 권한을 가진 사용자에게만 허용
                         .anyRequest().authenticated()); // 그 외의 모든 요청은 인증된 사용자만 접근가능
+
+        // 시큐리티가 동작할 때 JWTFilter 할 수 있도록
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
 
         // 필터 등록, at이 붙은 이유는 그 자리에 등록한다는 것
         // 로그인 필터를 등록하고, 그 위치를 어디로 할 것인지 2개 인자를 넣음
--- security-back/src/main/java/com/example/securityback/jwt/JWTFilter.java
+++ security-back/src/main/java/com/example/securityback/jwt/JWTFilter.java
@@ -1,5 +1,77 @@
 package com.example.securityback.jwt;
 
-public class JWTFilter {
+import com.example.securityback.dto.CustomUserDetails;
+import com.example.securityback.entity.UserEntity;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+
+public class JWTFilter extends OncePerRequestFilter {
+
+    private final JWTUtil jwtUtil;
+
+    public JWTFilter(JWTUtil jwtUtil) {
+
+        this.jwtUtil = jwtUtil;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // request에서 Authorization 헤더를 찾음
+        String authorization= request.getHeader("Authorization");
+
+        // Authorization 헤더 검증, authorization 에 토큰이 담겼는지를 확인하는 것
+        // authorization 가 null 이거나 Bearer 로 시작하지 않는다면
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+
+            System.out.println("token null");
+            filterChain.doFilter(request, response);
+
+            // 조건이 해당되면 메소드 종료 (필수)
+            return;
+        }
+
+        System.out.println("authorization now");
+        // Bearer 부분 제거 후 순수 토큰만 획득
+        String token = authorization.split(" ")[1]; // split 한 후 index 가 1 인 String 을 token 에 넣는 것
+
+        // 토큰 소멸 시간 검증
+        if (jwtUtil.isExpired(token)) {
+
+            System.out.println("token expired");
+            filterChain.doFilter(request, response);
+
+            // 조건이 해당되면 메소드 종료 (필수)
+            return;
+        }
+
+        // 토큰에서 username과 role 획득
+        String username = jwtUtil.getUsername(token);
+        String role = jwtUtil.getRole(token);
+
+        // userEntity를 생성하여 값 set
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(username);
+        userEntity.setPassword("temppassword"); // 비밀번호는 토큰에 담겨있지 않았어서 매번 DB 에 비밀번호를 조회하지 않고 임시 값으로 넣어줌
+        userEntity.setRole(role);
+
+        // UserDetails에 회원 정보 객체 담기
+        CustomUserDetails customUserDetails = new CustomUserDetails(userEntity);
+
+        // 스프링 시큐리티 인증 토큰 생성
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+        // 시큐리티 세션에 사용자 일시적으로 등록
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
 }
